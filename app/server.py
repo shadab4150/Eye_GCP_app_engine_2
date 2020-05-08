5b0ffacd4580a1c6150c25b3582398d5f05c31b8
shadab sayeed <37144992+shadab4150@users.noreply.github.com>
2020-05-09 00:16:55 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -1,14 +1,17 @@
+import aiohttp
+import asyncio
+import uvicorn
+from fastai import *
+from fastai.vision import *
+from io import BytesIO
 from starlette.applications import Starlette
+from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
-from starlette.middleware.cors import CORSMiddleware
-import uvicorn, aiohttp, asyncio
-from io import BytesIO
 
-from fastai.vision import *
+export_file_url = 'https://drive.google.com/uc?export=download&id=1-2vMyvg8mBOpPAtes3C87IqJJeR3n0WX'
+export_file_name = 'eye_first_714.pkl'
 
-model_file_url = 'https://drive.google.com/uc?export=download&id=1-2vMyvg8mBOpPAtes3C87IqJJeR3n0WX'
-model_file_name = 'eye_first_714.pkl'
 classes = ['Normal', 'Diabetic', 'Glaucoma', 'Cataract', 'AMD', 'Hypertension', 'Myopia','Other_abnormalities']
 path = Path(__file__).parent
 
@@ -16,36 +19,48 @@ app = Starlette()
 app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
 app.mount('/static', StaticFiles(directory='app/static'))
 
+
 async def download_file(url, dest):
     if dest.exists(): return
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             data = await response.read()
-            with open(dest, 'wb') as f: f.write(data)
+            with open(dest, 'wb') as f:
+                f.write(data)
+
 
 async def setup_learner():
-    await download_file(model_file_url, path/'models'/f'{model_file_name}.pth')
-    data_bunch = ImageDataBunch.single_from_classes(path, classes,
-        ds_tfms=get_transforms(), size=224).normalize(imagenet_stats)
-    learn = cnn_learner(data_bunch, models.resnet34, pretrained=False)
-    learn.load(model_file_name)
-    return learn
+    await download_file(export_file_url, path / export_file_name)
+    try:
+        learn = load_learner(path, export_file_name)
+        return learn
+    except RuntimeError as e:
+        if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
+            print(e)
+            message = "\n\nThis model was trained with an old version of fastai and will not work in a CPU environment.\n\nPlease update the fastai library in your training environment and export your model again.\n\nSee instructions for 'Returning to work' at https://course.fast.ai."
+            raise RuntimeError(message)
+        else:
+            raise
+
 
 loop = asyncio.get_event_loop()
 tasks = [asyncio.ensure_future(setup_learner())]
 learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
 loop.close()
 
+
 @app.route('/')
-def index(request):
-    html = path/'view'/'index.html'
-    return HTMLResponse(html.open().read())
+async def homepage(request):
+    html_file = path / 'view' / 'index.html'
+    return HTMLResponse(html_file.open().read())
+
 
 @app.route('/analyze', methods=['POST'])
 async def analyze(request):
-    data = await request.form()
-    img_bytes = await (data['file'].read())
+    img_data = await request.form()
+    img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
+    
     p = learn.predict(img)[2].numpy()
     idx = []
     percent = []
@@ -57,6 +72,7 @@ async def analyze(request):
     pred = " ".join(str(x) for x in pred)
     return JSONResponse({'result': str(pred)})
 
-if __name__ == '__main__':
-    if 'serve' in sys.argv: uvicorn.run(app, host='0.0.0.0', port=8080)
 
+if __name__ == '__main__':
+    if 'serve' in sys.argv:
+        uvicorn.run(app=app, host='0.0.0.0', port=5000, log_level="info")
